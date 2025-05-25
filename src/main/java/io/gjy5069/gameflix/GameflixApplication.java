e3ad7b7d41f3fb7b08322fd726118852f64d6d72
G. J. Yeomans <n8hth@protonmail.com>
2025-05-24 21:15:06 -0400

    Initial commit of working gameflix app

---
+++ src/main/java/io/gjy5069/gameflix/GameflixApplication.java
@@ -0,0 +1,13 @@
+package io.gjy5069.gameflix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameflixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameflixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/gjy5069/gameflix/GameflixApplicationTests.java
@@ -0,0 +1,13 @@
+package io.gjy5069.gameflix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameflixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
